3a01567ce261c46277636a42ab7ab5b4d391f8f9
Ender343 <131382987+Ender343@users.noreply.github.com>
2023-10-12 11:02:27 -0600

    Agrega campos adicionales a User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellidos')->nullable();
+            $table->string('curp')->nullable();
+            $table->string('rfc')->nullable();
+            $table->string('telefono')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('grado_estudios')->nullable();
+            $table->string('titulo')->nullable();
+            $table->string('cedula')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
