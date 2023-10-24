aa48d0e895a9cf0e9e670322e97b0f1af4bc9a22
j2deme <j2deme@gmail.com>
2023-10-24 11:43:31 -0600

    Agrega catálogo de sectores y subsectores
    Agrega los modelos y migraciones para las tablas de catálogo
    de sectores y subsectores.

---
+++ database/migrations/2023_10_24_174126_create_subsectores_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subsectores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreignId('sector_id')->constrained('sectores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subsectors');
+    }
+};
