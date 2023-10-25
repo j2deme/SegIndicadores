f8cc4ed9608ab9ed1e07bcc020dbb8cbf1e5027f
Ender343 <131382987+Ender343@users.noreply.github.com>
2023-10-25 10:23:46 -0600

    Agrega relaciones
    se agregan las relaciones campos sector y subsectores

--- app/Models/Registro.php
+++ app/Models/Registro.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Registro extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the Registro
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the sector that owns the Registro
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sector(): BelongsTo
+    {
+        return $this->belongsTo(Sector::class);
+    }
 }
+ 
