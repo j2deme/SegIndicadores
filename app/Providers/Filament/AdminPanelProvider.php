c48b3ced22bb0d2172bb6006c832cb3855b118ac
j2deme <j2deme@gmail.com>
2023-11-11 14:26:49 -0600

    🏗️ Refactoriza carga de recursos en el panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,12 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
-            ->resources([
-                \App\Filament\Resources\UserResource::class,
-                \App\Filament\Resources\DepartamentoResource::class,
-                \App\Filament\Resources\RegistroResource::class,
-            ])
-            //->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
