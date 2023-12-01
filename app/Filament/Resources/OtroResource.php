ce8e5129215fd16773873dd21b93d607769f528e
Ender343 <131382987+Ender343@users.noreply.github.com>
2023-12-01 11:37:29 -0600

    acomodo producto otros

--- app/Filament/Resources/OtroResource.php
+++ app/Filament/Resources/OtroResource.php
@@ -37,14 +37,18 @@ class OtroResource extends Resource
             ->schema([
                 Forms\Components\Hidden::make('user_id')
                 ->default(auth()->user()->id),
-                Forms\Components\TextInput::make('titulo')
-                    ->required()
-                    ->maxLength(255)
-                    ->label('Título'),
+                
                  Forms\Components\Section::make('Información de Registro')
                     ->relationship('registro')
                     ->schema(RegistroResource::form($form)->getComponents())
                     ->columns(2),
+                    Forms\Components\Section::make('Información Adicional')
+                    ->schema([
+                Forms\Components\TextInput::make('titulo')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Título'),
+                    ]),
              
             ]);
     }
