22c845a23be787e4b0e666fdb827eb8fb38fb161
Lujam2299 <21690001@tecvalles.mx>
2024-10-20 16:12:12 -0600

    Listado de Registros para continuar posteriormente con la exportacion de datos

--- app/Filament/Pages/Reportes.php
+++ app/Filament/Pages/Reportes.php
@@ -4,22 +4,31 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 use App\Models\Registro;
+use App\Models\Departamento;
 
 class Reportes extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
+    //protected static ?string $heading = null;
+    protected static ?string $navigationIcon = 'heroicon-s-bookmark-square';
     protected static string $view = 'filament.pages.reportes';
+    public $selectedFilter = '';
 
     public $registros;
-    public function mount()
+
+    public function getHeading():string
     {
-        $this->registros = Registro::join('users', 'registros.user_id', '=', 'users.id')
+        $depaId=auth()->user()->departamento_id;
+        $departamento = Departamento::find($depaId);
+
+        return 'Registros del departamento de ' . $departamento->nombre;
+    }
+        public function getRegistros()
+    {
+        $registros = Registro::join('users', 'registros.user_id', '=', 'users.id')
             ->where('users.departamento_id', auth()->user()->departamento_id)
+            ->select('registros.*', 'users.name as user_name', 'users.apellidos as user_apellidos')
             ->get();
 
-         dd($this->registros);
-
+        return $registros;
     }
-
 }
